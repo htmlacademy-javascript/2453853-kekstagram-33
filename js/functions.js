ed51d82be85ede3c42b5643dba77145b7a9f7e95
Dmitry Cherkashin <dchpost@mail.ru>
2024-10-22 10:10:41 +1000

    поправляет функции по замечаниям наставника

--- js/functions.js
+++ js/functions.js
@@ -1,80 +1,59 @@
-const example18 = 'Привет, здесь 18с.';
-const example7 = 'Привет!';
+// Проверка строки на максимальную длину
+const isMaxLength = function (currentString, maxLength) {
+  return currentString.lenght <= maxLength;
+};
+isMaxLength('Здесь 17 символов', 20);
 
+// Проверка, является ли строка полиндромом, 1 вариант
+function isPolindrom(currentString) {
+  const changeString = currentString.replaceAll(' ', '').toUpperCase(currentString);
+  let reverseString = '';
+  for (let i = changeString.length - 1; i >= 0; i--) {
+    reverseString += changeString.at(i);
+  }
+  return (changeString === reverseString) ? 'true' : 'false';
+}
 
-/* Строка короче 20 символов
-имяФункции('проверяемая строка', 20); // true*/
-/*const fun20 = (a, b) => {
-  if (a.length < b) {
-    return console.log('true');
+// Проверка, является ли строка полиндромом, 2 вариант
+function isPolindrom2(currentString) {
+  const changeString = currentString.replaceAll(' ', '').toLowerCase(currentString);
+  const reverseString = changeString.split('').reverse().join('');
+  return (changeString === reverseString) ? 'true' : 'false';
+}
+isPolindrom('топот'); // true
+isPolindrom2('ДовОд'); // true
+isPolindrom2('Кекс'); // false
+
+// Склеивание цифр в одно число
+// Приводим любое принятое значение к строке
+function mergeNumbers(currentString) {
+  let changeString;
+  if (typeof (currentString) === 'number') {
+    changeString = String(currentString);
   } else {
-    return console.log('false');
+    changeString = currentString.replaceAll(' ', '');
   }
-};*/
-/*const fun20 = (a, b) => {
-  if (a.length < b) {
-    return console.log('true');
-  } return console.log('false');
-};*/
-
-function funLess20(a, b) {
-  return (a.length < b) ? console.log('true') : console.log('false');
-}
-funLess20(example18, 20);
-
-/* Длина строки ровно 18 символов
-имяФункции('проверяемая строка', 18); // true*/
-function funEqual18(a, b) {
-  return (a.length === b) ? console.log('true') : console.log('false');
-}
-funEqual18(example18, 18);
-
-/* Строка длиннее 10 символов
-имяФункции('проверяемая строка', 10); // false */
-function funMore10(a, b) {
-  return (a.length > b) ? console.log('true') : console.log('false');
-}
-funMore10(example7, 10);
-
-
-/* Строка является палиндромом
-имяФункции('топот'); // true */
-function palindrom(a) {
-  a = a.replaceAll(' ', '').toUpperCase(a);
-  let b = '';
-  for (let i = a.length - 1; i >= 0; i--) {
-    b += a.at(i);
+  let finishedNumber = '';
+  for (let i = 0; i <= changeString.length; i++) {
+    if (!isNaN(Number(changeString[i]))) {
+      finishedNumber += changeString[i];
+    }
   }
-  return (a === b) ? console.log('true') : console.log('false');
+  return finishedNumber === '' ? NaN : Number(finishedNumber);
 }
-palindrom('топот');
+mergeNumbers('2023 год'); // 2023
+mergeNumbers('ECMAScript 2022'); // 2022
+mergeNumbers('1 кефир, 0.5 батона'); // 105
+mergeNumbers('агент 007'); // 7
+mergeNumbers('а я томат'); // NaN
 
-/* Несмотря на разный регистр, тоже палиндром
-имяФункции('ДовОд'); // true */
-palindrom('ДовОд');
-
-/* Это не палиндром
-имяФункции('Кекс');  // false */
-palindrom('Кекс');
-
-//Дополнительное задание
-/*Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN
-имяФункции('2023 год');            // 2023
-имяФункции('ECMAScript 2022');     // 2022
-имяФункции('1 кефир, 0.5 батона'); // 105
-имяФункции('агент 007');           // 7
-имяФункции('а я томат');           // NaN */
-
-function funAdditional(a) {
-  /* if (!Number.isNaN(a)) {
-    return console.log(parseFloat(a));
+// решение наставника
+function extraDigit(input) {
+  const str = String(input);
+  const digits = str.match(/\d+/g);
+  if (!digits) {
+    return NaN;
   }
-  return console.log('NaN'); */
-  const b = a.replaceAll(' ', '');
-  return (!Number.isNaN(a)) ? console.log(b, parseFloat(b)) : console.log('NaN');
+  return Number(digits.join(''));
 }
-funAdditional('2023 год');
-funAdditional('ECMAScript 2022');
-funAdditional('1 кефир, 0.5 батона');
-funAdditional('агент 007');
-funAdditional('а я томат');
+extraDigit('2023 год');
