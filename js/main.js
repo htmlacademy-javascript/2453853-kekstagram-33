145fc90f329c732b5c7d96d8ef951fc9de276902
Dach1980 <145884906+Dach1980@users.noreply.github.com>
2024-10-23 20:42:25 +1000

    добавляет comments

--- js/main.js
+++ js/main.js
@@ -25,14 +25,17 @@ const MESSAGES = [
 ];
 
 // Возможные описания фотографий
-const MESSAGES = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+const DESCRIPTIONS = [
+  'Закат над морем.',
+  'Горный пейзаж.',
+  'Семейный пикник.',
+  'Морская прогулка.',
+  'Техно-арт.',
+  'На отдыхе.'
 ];
+
+// Количество сгенерированных объектов
+const SIMILAR_USER_COUNT = 25;
 
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
@@ -41,25 +44,18 @@ const getRandomInteger = (a, b) => {
   return Math.floor(result);
 };
 
-//	id, число от 1 до 25.
-// url, строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} — это число от 1 до 25
-// comments, массив объектов — список комментариев, оставленных другими пользователями к этой фотографии. Количество комментариев к каждой фотографии — случайное число от 0 до 30
+const userCommentObject = () => ({
+  id: getRandomInteger(1, 25),
+  url: String('photos/' + getRandomInteger(1, 25) + '.jpg'),
+  description: DESCRIPTIONS[getRandomInteger(0, DESCRIPTIONS.length - 1)],
+  comments: {
+    commentId: getRandomInteger(1, 25), // ? повторить значение id
+    avatar: String('img/avatar-' + getRandomInteger(1, 6) + '.svg'),
+    message: MESSAGES[getRandomInteger(0, MESSAGES.length - 1)], // ? случайно выбрать одно-два сообщения
+    name: NAMES[getRandomInteger(0, NAMES.length - 1)],
+  }
+});
 
-const createComment = () => {
-  const randomIdIndex = getRandomInteger(1, 25);
-  const randomUrl = getRandomInteger(1, 25);
-  const randomNameIndex = getRandomInteger(0, NAMES.length - 1);
-  const randomMessageIndex = getRandomInteger(0, MESSAGES.length - 1);
+const similarUserObject = Array.from({ length: SIMILAR_USER_COUNT }, userCommentObject);
 
-  return {
-    id: randomIdIndex,
-    url: randomUrl,
-    avatar: 'photos/' + randomIdIndex + '.jpg',
-    message: MESSAGES[randomMessageIndex],
-    name: NAMES[randomNameIndex],
-  };
-};
-
-console.log(
-  createComment()
-);
+similarUserObject();
