b28ff019a4307633ed7929dfac3b0efdb123eea2
Dach1980 <145884906+Dach1980@users.noreply.github.com>
2024-11-22 20:32:28 +1000

    удаляет закомментированный текст

--- js/load-file.js
+++ js/load-file.js
@@ -1,14 +1,3 @@
-/*
- <!-- Изначальное состояние поля для загрузки изображения -->
-            <fieldset class="img-upload__start">
-              <input type="file" id="upload-file" class="img-upload__input  visually-hidden" name="filename" required>
-              <label for="upload-file" class="img-upload__label  img-upload__control">Загрузить</label>
-            </fieldset>
-<!-- Предварительный просмотр изображения -->
-                  <div class="img-upload__preview">
-                    <img src="img/upload-default-image.jpg" alt="Предварительный просмотр фотографии">
-                  </div>
-*/
 const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
 const fileChooser = document.querySelector('.img-upload__input');
