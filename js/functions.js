6708079252b9b277d1ecea02310839149145889c
Dmitry Cherkashin <dchpost@mail.ru>
2024-11-29 16:42:21 +1000

    коментирует функцию

--- js/functions.js
+++ js/functions.js
@@ -87,3 +87,4 @@ console.log(isValidTimeMeeting('8:0', '10:0', '8:0', 120));
 console.log(isValidTimeMeeting('08:00', '14:30', '14:00', 90));
 console.log(isValidTimeMeeting('14:00', '17:30', '08:0', 90));
 console.log(isValidTimeMeeting('8:00', '17:30', '08:00', 900));
+*/
